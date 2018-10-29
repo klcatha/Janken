1f83fe326029d318b969a12662a861b78202b47d
klcatha <klcatha0518@gmail.com>
2018-10-29 11:05:56 +0900

    transition result activity

--- app/src/main/java/com/example/kengomaruyama/janken/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/janken/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        gu.setOnClickListener{ onJankenButtonTapped(it) }
+        choki.setOnClickListener{ onJankenButtonTapped(it) }
+        pa.setOnClickListener { onJankenButtonTapped(it) }
     }
 
     fun onJankenButtonTapped(view: View?){
