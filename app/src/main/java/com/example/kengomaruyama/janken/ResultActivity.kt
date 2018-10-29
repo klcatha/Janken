9bd996484f53da1aa6a6a8078dd6480294d70c87
klcatha <klcatha0518@gmail.com>
2018-10-29 11:16:57 +0900

    my hand image change by id

--- app/src/main/java/com/example/kengomaruyama/janken/ResultActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/janken/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.kengomaruyama.janken
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
@@ -9,5 +10,11 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
         val id = intent.getIntExtra("MY_HAND",0)
+
+        when(id){
+            R.id.gu -> myHandImage.setImageResource(R.drawable.gu)
+            R.id.choki -> myHandImage.setImageResource(R.drawable.choki)
+            R.id.pa -> myHandImage.setImageResource(R.drawable.pa)
+        }
     }
 }
