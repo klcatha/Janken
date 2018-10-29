a68bcbe7dc3d61b4297b4ed325ac4945c48dcf7b
klcatha <klcatha0518@gmail.com>
2018-10-29 11:01:43 +0900

    add on janken button tapped method

--- app/src/main/java/com/example/kengomaruyama/janken/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/janken/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.kengomaruyama.janken
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
+    }
+
+    fun onJankenButtonTapped(view: View?){
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent);
     }
 }
