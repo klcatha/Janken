36adf1a0fa712b95747114096d69d12bab115d31
klcatha <klcatha0518@gmail.com>
2018-11-02 12:08:19 +0900

    add saveData method

--- app/src/main/java/com/example/kengomaruyama/janken/ResultActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/janken/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.kengomaruyama.janken
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
@@ -49,5 +50,26 @@ class ResultActivity : AppCompatActivity() {
         }
 
         backButton.setOnClickListener { finish() }
+    }
+
+    private fun saveData(myHand: Int, comHand: Int, gameResult: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val gameCount = pref.getInt("GAME_COUNT", 0)
+        val winningStreakCount = pref.getInt("WINNING_STREAK_COUNT",0)
+        val lastComHand = pref.getInt("LAST_COM_HAND",0)
+        val lastGameResult = pref.getInt("GAME_RESULT",-1)
+
+        val editor = pref.edit()
+        editor.putInt("GAME_COUNT",gameCount + 1)
+                .putInt("WINNING_STREAK_COUNT",
+                        if (lastGameResult == 2 && gameResult == 2)
+                winningStreakCount + 1
+                else
+                0)
+                .putInt("LAST_MY_HAND", myHand)
+                .putInt("LAST_COM_HAND", comHand)
+                .putInt("BEFORE_LAST_COM_HAND", lastComHand)
+                .putInt("GAME_RESULT", gameResult)
+                .apply()
     }
 }
